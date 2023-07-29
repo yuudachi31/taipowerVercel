4eea1ddc3dad25a4e8097f7c8ed757aef2c54f2f
yuudachi31 <43200967+yuudachi31@users.noreply.github.com>
2023-07-29 14:03:12 +0800

    login test

--- src/api/frontApi.js
+++ src/api/frontApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = `http://localhost:5000/api/v1/`
+// const baseURL = `http://localhost:5000/api/v1/`
+const baseURL = `https://taipower.azurewebsites.net/`
 const token = document.cookie.split('; ').find(row => row.startsWith('fltk')) ? true : false;
 
 
@@ -8,22 +9,24 @@ const token = document.cookie.split('; ').find(row => row.startsWith('fltk')) ? 
 export const postUser = async (username, password) => {
     try {
         console.log('in pos');
-        const _url = `${baseURL}user/signin`
+        const _url = `${baseURL}security/login`
         const result = await axios.post(_url, {
-            "username": username,
-            "password": password
+            "user_id": username,
+            "passwd": password
         }, {
             headers: {
-                'Accept': 'application/json'
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
             }
         })
+        console.log(result)
         if (result.status === 200) {
             return result.data
         }
     } catch (err) {
         console.log(err.response);
-        return { "errStatus": err.response.status, "errDetail": err.response.data.detail }
-    }
+        // return { "errStatus": err.response.status, "errDetail": err.response.data.detail }
+    } 
 }
 
 // 包含 token 的
