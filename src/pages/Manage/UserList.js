c5542f2119cc4d39529d3d3196f7bbaed8c94420
shakuneko <56393184+shakuneko@users.noreply.github.com>
2023-11-14 16:16:02 +0800

    feat:  UserList_identityFilter

--- src/pages/Manage/UserList.js
+++ src/pages/Manage/UserList.js
@@ -1,6 +1,6 @@
 //帳號管理
 //antd
-import { Divider, Layout, Input } from 'antd';
+import { Divider, Layout, Input, Table } from 'antd';
 import { DownOutlined, SearchOutlined } from '@ant-design/icons';
 import { Dropdown, Space } from 'antd';
 import { useState } from 'react';
@@ -16,64 +16,67 @@ export const USER_DATA = [
   {
     user_id: 0,
     name: 'User_001',
-    group: ['區處管理者', '運維人員'],
+    group: ['區處管理員', '區處操作員'],
     email: 'aaa123@gmail.com'
   },
   {
     user_id: 1,
     name: 'User_002',
-    group: ['運維人員'],
+    group: ['區處操作員'],
     email: 'aaa123@gmail.com'
   },
   {
     user_id: 2,
     name: 'User_003',
-    group: ['區處檢修人員'],
+    group: ['區處管理員'],
     email: 'aaa123@gmail.com'
   },
   {
     user_id: 3,
     name: 'User_004',
-    group: ['區處檢修人員'],
+    group: ['區處管理員'],
     email: 'aaa123@gmail.com'
   },
   {
     user_id: 4,
     name: 'User_005',
-    group: ['區處管理者', '運維人員'],
+    group: ['區處管理員', '區處操作員'],
     email: 'aaa123@gmail.com'
   },
   {
     user_id: 5,
     name: 'User_006',
-    group: ['區處管理者', '運維人員'],
+    group: ['區處管理員', '區處操作員'],
     email: 'aaa123@gmail.com'
   },
   {
     user_id: 6,
     name: 'User_007',
-    group: ['區處管理者', '運維人員'],
+    group: ['區處管理員', '區處操作員'],
     email: 'aaa123@gmail.com'
   },
   {
     user_id: 7,
     name: 'User_008',
-    group: ['區處管理者', '運維人員'],
+    group: ['區處管理員', '區處操作員'],
     email: 'aaa123@gmail.com'
   },
   {
     user_id: 8,
     name: 'User_009',
-    group: ['區處管理者', '運維人員'],
+    group: ['區處管理員', '區處操作員'],
     email: 'aaa123@gmail.com'
   },
   {
     user_id: 9,
     name: 'User_010',
-    group: ['區處管理者', '運維人員'],
+    group: ['區處管理員', '區處操作員'],
     email: 'aaa123@gmail.com'
   },
 ];
+const onChange = (pagination, filters, sorter, extra) => {
+  console.log('params', pagination, filters, sorter, extra);
+};
 
 function UserList() {
   const _history = useHistory()
@@ -85,7 +88,49 @@ function UserList() {
   const onSearch = () => {
     console.log("aa")
   };
-
+  
+  const columns = [
+    {
+      title:'帳號名稱',
+      key: 'account_name',
+      dataIndex: 'name',
+    },
+    {
+      title:'身份權限',
+      key: 'identity',
+      dataIndex: 'group', // 修改这里为 'group'
+      render: groups => groups.join(' / '), // 使用render方法将数组转换为字符串
+      filters: [
+        {
+          text: '總處管理員',
+          value: '總處管理員',
+        },
+        {
+          text: '總處操作員',
+          value: '總處操作員',
+        },
+        {
+          text: '區處管理員',
+          value: '區處管理員',
+        },
+        {
+          text: '區處操作員',
+          value: '區處操作員',
+        },
+      ],
+      onFilter: (value, record) => record.group.includes(value),
+    },
+    {
+      title:'編輯',
+      key: 'edit',
+      dataIndex: 'edit',
+      render: (text, record) => (
+        <button class="btn-manage justify-self-end mr-4" onClick={() => { _history.push(`/manage/user/${record.user_id}`) }}>
+          編輯
+        </button>
+      ),
+    },
+  ]
   return (
     <Layout class="px-20 py-12 manage-wrapper bg-gray-100">
       <Header class="pt-4 pb-8 flex space-x-7 items-center">
@@ -99,24 +144,25 @@ function UserList() {
       </Header>
       <Content>
         <Layout>
-          <Header class="pl-16 user-grid-row h-14 bg-purple-400 text-white font-medium text-base">
-            <div class="col-span-1">帳號名稱</div>
-            <div>身份權限</div>
+          {/* <Header class="pl-16 user-grid-row h-14 bg-purple-400 text-white font-medium text-base"> */}
+          <Table columns={columns} dataSource={USER_DATA}  onChange={onChange} pagination={{ defaultCurrent: 1, total: 50 }}  />
+            {/* <div class="col-span-1">帳號名稱</div>
+            <div>身份權限</div> */}
             {/* <Dropdown menu={{ items }}>
-        <a onClick={(e) => e.preventDefault()}>
-          <Space>
-            Hover me
-            <DownOutlined />
-          </Space>
-        </a>
-      </Dropdown> */}
-          </Header>
-          <Content class="px-8 py-7">
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                Hover me
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown> */}
+          {/* </Header> */}
+          {/* <Content class="px-8 py-7">
             {USER_DATA.map((user) => <UserItem user={user} />)}
             <div class="flex justify-end py-3 border-purple-400">
               <Pagination defaultCurrent={1} total={50} />
             </div>
-          </Content>
+          </Content> */}
         </Layout>
       </Content>
     </Layout>
@@ -125,18 +171,18 @@ function UserList() {
 }
 export default UserList;
 
-function UserItem({ user }) {
-  const _history = useHistory()
+// function UserItem({ user }) {
+//   const _history = useHistory()
 
-  return (
-    <>
-      <div class="user-grid-row pt-1">
-        <div class="col-span-1">{user.name}</div>
-        <div class="col-span-2">{user.group.join(' / ')}</div>
-        <button class="btn-manage justify-self-end mr-4" onClick={() => { _history.push(`/manage/user/${user.user_id}`) }}>編輯</button>
-      </div>
-      <Divider />
-    </>
-  );
+//   return (
+//     <>
+//       <div class="user-grid-row pt-1">
+//         <div class="col-span-1">{user.name}</div>
+//         <div class="col-span-2">{user.group.join(' / ')}</div>
+//         <button class="btn-manage justify-self-end mr-4" onClick={() => { _history.push(`/manage/user/${user.user_id}`) }}>編輯</button>
+//       </div>
+//       <Divider />
+//     </>
+//   );
 
-}
+// }
