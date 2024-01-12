61a3f9dc410ab62c202283d8999a9772388717fa
shakuneko <56393184+shakuneko@users.noreply.github.com>
2024-01-13 02:02:15 +0800

    feat: IndustrtInfo_Chart

--- src/pages/Transformer/IndustryInfo.js
+++ src/pages/Transformer/IndustryInfo.js
@@ -1,12 +1,11 @@
 //antd
 import { Layout, Divider, DatePicker, Progress, TimePicker } from 'antd';
-
 import { MessageOutlined, CaretRightOutlined, CaretLeftOutlined } from '@ant-design/icons';
 import { red, green, lime, yellow, orange, volcano } from '@ant-design/colors';
 import styles from '../../index.less'
 import moment from 'moment';
 import { saveDailyRates, saveQuarterRates, saveMonthlyRates, saveEachTransInfo } from '../../actions/transformer'
-import data from '../../pages/Transformer/testdata.json';
+import testdata from '../../pages/Transformer/testdata.json';
 // import EChartRate from '../../components/chart/EChartRate';
 import { data_main, data_month } from '../../components/chart/TempData'
 import { getDailyRates, getQuarterRates, getMonthlyRates, getEachTransformer } from '../../api/frontApi'
@@ -14,9 +13,7 @@ import { connect } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import queryString from "query-string";
-
-import { ResponsiveLine } from '@nivo/line'
-
+import IndustryInfoChart from '../../components/chart/IndustryInfoChart'
 const { Header, Sider, Content } = Layout;
 
 const Timeformat = 'HH:mm';
@@ -164,110 +161,7 @@ function TRInfo({ transformer, saveDailyRates, saveQuarterRates, saveMonthlyRate
 
       </Layout>
       <Divider />
-      <ResponsiveLine
-        data={data}
-        margin={{ top: 50, right: 160, bottom: 50, left: 60 }}
-        xScale={{ type: 'linear' }}
-        yScale={{ type: 'linear', stacked: true, min: 0, max: 2500 }}
-        yFormat=" >-.2f"
-        curve="monotoneX"
-        axisTop={null}
-        axisRight={{
-            tickValues: [
-                0,
-                500,
-                1000,
-                1500,
-                2000,
-                2500
-            ],
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            format: '.2s',
-            legend: '',
-            legendOffset: 0
-        }}
-        axisBottom={{
-            tickValues: [
-                0,
-                20,
-                40,
-                60,
-                80,
-                100,
-                120
-            ],
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            format: '.2f',
-            legend: 'price',
-            legendOffset: 36,
-            legendPosition: 'middle'
-        }}
-        axisLeft={{
-            tickValues: [
-                0,
-                500,
-                1000,
-                1500,
-                2000,
-                2500
-            ],
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            format: '.2s',
-            legend: 'volume',
-            legendOffset: -40,
-            legendPosition: 'middle'
-        }}
-        enableGridX={false}
-        colors={{ scheme: 'spectral' }}
-        lineWidth={1}
-        pointSize={4}
-        pointColor={{ theme: 'background' }}
-        pointBorderWidth={1}
-        pointBorderColor={{ from: 'serieColor' }}
-        pointLabelYOffset={-12}
-        useMesh={true}
-        gridXValues={[ 0, 20, 40, 60, 80, 100, 120 ]}
-        gridYValues={[ 0, 500, 1000, 1500, 2000, 2500 ]}
-        legends={[
-            {
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 140,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemDirection: 'left-to-right',
-                itemWidth: 80,
-                itemHeight: 12,
-                itemOpacity: 0.75,
-                symbolSize: 12,
-                symbolShape: 'circle',
-                symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemBackground: 'rgba(0, 0, 0, .03)',
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
-    />
-
-      <Divider />
-     
-
-
-      <Divider />
-      
+      <IndustryInfoChart />   
     </Layout>
   );
 
