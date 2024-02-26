21d54d604efb9991dbb979f15e397d774732a5c9
Singularity-v <48972803+Singularity-v@users.noreply.github.com>
2024-02-26 13:57:54 +0800

    style: larger the title

--- src/pages/Manage/UserInfo.js
+++ src/pages/Manage/UserInfo.js
@@ -232,7 +232,7 @@ console.log(user)
 
   return (
     <Layout class="h-screen px-20 py-12 manage-wrapper bg-gray-100">
-      <Header class="pt-4 pb-8 grid grid-rows-2 auto-cols-min items-center gap-x-7">
+      <Header class="pt-4 pb-8 grid grid-rows-2 auto-cols-max items-center gap-x-7">
         <a onClick={(e) => { e.preventDefault(); _history.push('/manage/user'); }} class="row-span-2"><LeftOutlined style={{ fontSize: '24px', color: '#7B7B7B' }} /></a>
         <span class="text-base text-gray-400 col-start-2 whitespace-nowrap overflow-hidden">帳號名稱</span>
         <h2 class="flex-auto font-bold text-xl col-start-2">{userManage.userList.find((el) => el.user_id == userId)?.name}</h2>
