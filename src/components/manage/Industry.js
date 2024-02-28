aa43f89f501d033f817adb6308d8942235de9b22
yuudachi31 <yuudachi31@gmail.com>
2024-02-29 00:05:52 +0800

    fix:qs

--- src/components/manage/Industry.js
+++ src/components/manage/Industry.js
@@ -3,25 +3,26 @@ import { Form, Input, Select, DatePicker, Space  } from 'antd';
 import { useEffect, useState } from 'react';
 import { saveDailyRates } from '../../actions/transformer'
 import moment from 'moment';
-import queryString from "query-string";
+// import queryString from "query-string";
 import { getDailyRates } from '../../api/frontApi'
+import qs from "qs";
 const { Option } = Select;
 const { RangePicker } = DatePicker;
 
 function Industry({ isEdited, user = null }) {
-  const parsed = queryString.parse(window.location.search);
+  const parsed = qs.parse(window.location.search);
   const [selectedYear, setSelectedYear] = useState(null);
   const [selectedMonth, setSelectedMonth] = useState(null);
   const [selectedDay, setSelectedDay] = useState(null);
   const monthFormat = 'YYYY 年 MM 月';
   const currentDate = new Date('2022/6/1');
   const handlePanelChange_daily =(value,mode)=>{
-    const parsed = queryString.parse(window.location.search);
+    const parsed = qs.parse(window.location.search);
     // setSelectedYear(value.year());
     if (mode === 'month') {
       setSelectedYear(value.year());
       setSelectedMonth(value.month());
-    // const parsed = queryString.parse(window.location.search);
+    // const parsed = qs.parse(window.location.search);
       console.log(value.year())
       getDailyRates(parsed.coor, parsed.div, parsed.tr_index, value.year(),value.month()+1).then((data) => {
         if (data.errStatus) {
