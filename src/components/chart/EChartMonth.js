2a9079b8282c76e4ff9c3a9388274c24db4baf3a
yuudachi31 <yuudachi31@gmail.com>
2024-03-27 02:34:02 +0800

    feat:monthChart

--- src/components/chart/EChartMonth.js
+++ src/components/chart/EChartMonth.js
@@ -1,41 +1,55 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 
-import { BarChart,ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Rectangle } from 'recharts';
+import { BarChart, ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Rectangle } from 'recharts';
 import { renderCustomXTick, renderCustomYLeftTick, renderCustomYRightTick } from './CustomRender'
 import queryString from "query-string";
 
-function EChartMain({data}) {
+function EChartMain({ data }) {
     const [ticks_y, setTicks_y] = useState(['0.00', '50.00', '100.00', '150.00', '200.00']);
 
     const processedData = data.map((item, index) => ({
         ...item,
-        fill: index < 6 ==='#92D131' ? '#55A630' : '#BDBDBD',
-        
-      }));
+        fill: index < 6 === '#92D131' ? '#55A630' : '#BDBDBD',
 
-    const renderLegend = ({ payload={} }) => {
-        if (payload?.length <=0 || payload==null) return null
-        else{
-           return (
-            <ul class="border-2 border-green-400 py-2 px-3 bg-white">
-                {payload[2].value==0?<><li key={`item-0`}>{`尖峰利用率：${payload[3].value}kW`}</li>
-                    <li key={`item-1`}>{`離峰利用率：${payload[0].value}kW`}</li></>:
-                    <>
-                    <li key={`item-0`}>{`利用率：${payload[2].value}%`}</li>
-                    </>}
-                
-                {/* <li key={`item-0`}>{`利用率：${payload[3].value}%`}</li> */}
-                {/* <li key={`item-0`}>{`尖峰：${payload[0].value}kW`}</li>
+    }));
+
+    const renderLegend = ({ payload = {} }) => {
+        if (payload?.length <= 0 || payload == null) return null
+        else {
+            return (
+                <ul class="border-2 border-green-400 py-2 px-3 bg-white">
+                    {payload[2]?.value == 0 ?
+                        <>
+                            {payload[3]!=undefined && payload[0] ? (
+                                <>
+                                    <li key={`item-0`}>{`尖峰利用率：${payload[3]?.value}kW`}</li>
+                                    <li key={`item-1`}>{`離峰利用率：${payload[0]?.value}kW`}</li>
+                                </>) : (<></>)}
+
+                        </>
+                        :
+                        <>
+
+                            {payload[2] ? (
+                                <>
+                                    <li key={`item-0`}>{`利用率：${payload[2]?.value}%`}</li>
+                                </>
+                            ) : (<></>)}
+                        </>
+                    }
+
+                    {/* <li key={`item-0`}>{`利用率：${payload[3].value}%`}</li> */}
+                    {/* <li key={`item-0`}>{`尖峰：${payload[0].value}kW`}</li>
                 <li key={`item-1`}>{`離峰：${payload[1].value}kW`}</li> */}
-            </ul>
-        ); 
+                </ul>
+            );
         }
-        
+
     }
     const history = useHistory();
     //Bar點擊
-    const BarClickToMonth = ({ payload={} }) => {
+    const BarClickToMonth = ({ payload = {} }) => {
         // 使用 React Router 导航
         const parsed = queryString.parse(window.location.search);
         console.log(payload)
@@ -48,18 +62,18 @@ function EChartMain({data}) {
                 <XAxis dataKey="x_key" tickLine={false} tick={renderCustomXTick} />
                 {/* ticks={ticks_y} */}
                 {/* dataKey="load_total" */}
-                <YAxis dataKey="uti_rate"  orientation="left" tickLine={false} tickCount={6} tick={renderCustomYLeftTick} />
+                <YAxis dataKey="uti_rate" orientation="left" tickLine={false} tickCount={6} tick={renderCustomYLeftTick} />
                 <CartesianGrid strokeDasharray="2" vertical={false} stroke="#BDBDBD" />
-                <Tooltip content={renderLegend} cursor={false}/>
-                <Bar isAnimationActive={false} dataKey="load_off" name="尖峰利用率" stackId="a" barSize={20} fill="#92D131" activeBar={{ fill: "#81C12E" }} onClick={BarClickToMonth}/>
-                <Bar isAnimationActive={false} dataKey="load_on_forChart" name="離峰利用率" stackId="a" barSize={20} fill="#55A630" activeBar={{ fill: "#4A8927"}} onClick={BarClickToMonth}/>
-                <Bar isAnimationActive={false} dataKey="predict_bars" name="預測利用率" stackId="a" barSize={20} fill="#BDBDBD" activeBar={{ fill: "#939393"}} onClick={BarClickToMonth}/>
-              {/* 這條透明bar是為了hover寫的 */}
-                <Bar isAnimationActive={false} dataKey="load_on" name="離峰利用率" stackId="a" barSize={20} fill="transparent"/>
+                <Tooltip content={renderLegend} cursor={false} />
+                <Bar isAnimationActive={false} dataKey="load_off" name="尖峰利用率" stackId="a" barSize={20} fill="#92D131" activeBar={{ fill: "#81C12E" }} onClick={BarClickToMonth} />
+                <Bar isAnimationActive={false} dataKey="load_on_forChart" name="離峰利用率" stackId="a" barSize={20} fill="#55A630" activeBar={{ fill: "#4A8927" }} onClick={BarClickToMonth} />
+                <Bar isAnimationActive={false} dataKey="predict_bars" name="預測利用率" stackId="a" barSize={20} fill="#BDBDBD" activeBar={{ fill: "#939393" }} onClick={BarClickToMonth} />
+                {/* 這條透明bar是為了hover寫的 */}
+                <Bar isAnimationActive={false} dataKey="load_on" name="離峰利用率" stackId="a" barSize={20} fill="transparent" />
                 {/* 圖表線 */}
-                <Line isAnimationActive={false} type="monotone"  name="尖峰利用率" dataKey="load_on" stroke="#BDBDBD" strokeWidth={2} activeDot={{ r: 5 }} dot={{ stroke: '#BDBDBD', strokeWidth: 2 }}  />
+                <Line isAnimationActive={false} type="monotone" name="尖峰利用率" dataKey="load_on" stroke="#BDBDBD" strokeWidth={2} activeDot={{ r: 5 }} dot={{ stroke: '#BDBDBD', strokeWidth: 2 }} />
 
-                
+
             </ComposedChart>
         </ResponsiveContainer>
     );
